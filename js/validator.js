4de3b14750a534cd2337ee4f8c144a35f0db5e5d
OscarVega4 <oacevedo@esde.net>
2024-10-24 20:36:45 -0500

    Secrea estructura basica de vista proyectos

--- js/validator.js
+++ js/validator.js
@@ -1,3 +1,3 @@
 import { registrarTiempos } from "../controllers/ControllerTiempos.js";
-
+import { listarProyectos,filtrarPorEmpresa,filtrarPorProyecto,filtrarPorTipoProyecto } from "../controllers/ControllerProyectos.js";
 document.getElementById('btnRegistrar').addEventListener('click', registrarTiempos) 
